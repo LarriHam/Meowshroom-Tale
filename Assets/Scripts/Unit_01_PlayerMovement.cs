d97cd2997247dd14e9bc40db9c31a98692f4dff3
LarriHam <125363490+LarriHam@users.noreply.github.com>
2024-12-04 13:41:31 +0000

    basic functionality

---
+++ Assets/Scripts/Unit_01_PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit_01_PLayerMovement: MonoBehaviour
+{
+    public float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalAxis, 0, verticalAxis);
+
+        Debug.Log(movementDirection);
+
+        // transform.position += movementDirection * Time.deltaTime * speed;
+
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+    }
+}
